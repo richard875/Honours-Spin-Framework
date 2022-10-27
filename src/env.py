f94ffd49a349285a1727190f6e3291e89f410941
Richard Lee <richard_875@me.com>
2022-10-28 09:41:16 +1100

    - Modify sorting index file (env)

--- src/env.py
+++ src/env.py
@@ -1,5 +1,6 @@
 import os
 import cgi
+from datetime import datetime
 
 from tests import main
 
@@ -8,6 +9,8 @@ print('Status: 200')
 print()
 
 
+now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+print("Run at:", now)
 print('Experiment 1: Benchmark')
 print('=======================')
 test, total_time = main()
